5013b89336de4b484f65ca12000544c1257f542e
Raghavendra519 <gutthularaghavendra2612@gmail.com>
2024-05-12 10:34:50 +0530

    Create Odd or even.java

---
+++ Odd or even.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    static String oddEven(int N) {
+
+        if (N % 2 == 0) {
+
+            return "even";
+
+        } else {
+
+            return "odd";
+
+        }
+
+    }
+
+}
